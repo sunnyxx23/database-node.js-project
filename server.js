81454322bbdbe6927c820bcf823e5d5a5f828d88
sunnyxx23 <nerdbuster123@gmail.com>
2023-03-30 10:51:54 +0200

    30/03 - fortsätt med att göra search funktionen, gör admin sida men börja inte men login än!!! Donera inte blodet till krigshjältar i sydamerika!!

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ var obj = {};
 
 
 app.get('/', function(req,res){
-   let sql = 'SELECT * FROM project ORDER BY id DESC';
-   db.query(sql, function(err, results){
+    res.render("post")
+/*   let sql = 'SELECT * FROM project ORDER BY id DESC';
+   db.query(sql, function(err, results){    
        if(err) {
            throw err;
        } else {
            obj = {data: results};
            res.render('index', obj)
        }
-   });
+   });*/
 });
-
 
 app.get('/post', function(req,res){
     res.render('post')
@@ -36,18 +36,46 @@ app.listen(process.env.PORT || 3000, function(){
    console.log('server, port 3000');
 });
 
+app.get('/search', function(req,res){
+    res.render('search')
+ });
 
-app.post('/post', upload.single('img'), function(req,res){
-   const title = req.body.title;
-   const text = req.body.text;
-   //const img = "";
-   const img = "/uploads/" + req.file.filename;
-   const sqlInstert = "INSERT INTO project (title, text, img) VALUES (?, ?, ?);"
-   db.query(sqlInstert, [title, text, img], (err, result)=> {
+app.post('/search', function(req, res){
+    let sql = 'SELECT * FROM project WHERE '+req.body.searchBy+' LIKE ?'
+
+    db.query(sql, '%'+req.body.searchQuery+'%', function(err, results){
+        if(err) {
+            throw err;
+        } else {
+            console.log(results);
+            console.log('Filtered content');
+            obj = {data: results};
+            res.render('index', obj);
+        }
+    });
+});
+
+
+app.post('/submitted', function(req,res){
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const pnummer = req.body.pnummer;
+    const btype = req.body.btype;
+    var bdis = "";
+    if(req.body.bdis == "on"){
+        bdis = 1;
+    } else {
+        bdis = 0;
+    }
+   
+   const sqlInstert = "INSERT INTO project (fname, lname, pnummer, btype, bdis) VALUES (?, ?, ?, ?, ?);"
+   
+   db.query(sqlInstert, [fname, lname, pnummer, btype, bdis], (err, result)=> {
        if(err) {
            throw err;
        } else {
-           res.send('poSt addud');
+           res.render('sucess');
        }
    });
+   
 });
